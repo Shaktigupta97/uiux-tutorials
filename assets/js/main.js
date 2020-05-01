af1585f3da3e09ea91a582f9bb3ba1026098ffe4
Shaktigupta97 <shaktigupta321@gmail.com>
2020-05-01 11:46:21 +0530

    Particle-js added

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,32 @@ $('#open-side-nav').on('click', function(){
   //   side_nav_ele.addClass('open');
   // }
 });
+
+function loadTemplate(){
+  /** var element = $(".class, #id, [attribute], tag"); */
+
+  var ajaxElements = $('[data-load-template]');
+
+  // Each loop
+  ajaxElements.each(function(index, element){
+    var theElement = $(element);
+
+    $.ajax({
+      method: "GET",
+      url: theElement.data('load-template'),
+      data: {}
+    }).done(function(response) {
+      theElement.html(response);
+    })
+    
+  });
+}
+
+loadTemplate();
+
+/** Load Particle JS Container */
+try{
+  particlesJS.load('particles-js', '/assets/js/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+} catch (err) {}
